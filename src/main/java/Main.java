1bbc5b012df932a48e08340fcf63f2d926baf2c3
DDG78 <Ddg78@mail.ru>
2023-03-28 23:07:16 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    BonusService service = new BonusService();
+
+    {
+
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+    }
+}
